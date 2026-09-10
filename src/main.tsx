23e3ae3dd992b986f31f70f349fc844688c06687
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-10 18:08:32 +0530

    Route legacy blog and game archives

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
-import { App } from './App';
+import App from './App';
+import { LegacyHubPage } from './LegacyHubPage';
+
+function Root() {
+  const path = window.location.pathname.replace(/\/+$/, '') || '/';
+
+  if (path === '/blog' || path.startsWith('/blog/')) {
+    return <LegacyHubPage kind="blog" />;
+  }
+
+  if (path === '/games' || path.startsWith('/games/')) {
+    return <LegacyHubPage kind="games" />;
+  }
+
+  return <App />;
+}
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
